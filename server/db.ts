72c2fa3f84fa47fe6eb3b5617564b7dcbfee7043
Mikey3832 <mikeyverberkmoes@gmail.com>
2026-09-24 01:27:07 -0400

    basically done now

--- server/db.ts
+++ server/db.ts
@@ -5,19 +5,19 @@ const TIME_LEFT = 8; // seconds
 const sortQuestions = (questions: { points: number; question: string; answer: string; imgSrc?: string; }[]) => questions.sort((a, b) => a.points - b.points).map(q => ({ ...q, answered: false, buzzers: [] as string[] }));
 const pastQuestions: Question[] = sortQuestions([
     {
-        points: 100,
-        question: 'what state do i live in?',
-        answer: 'new york',
-    },
-    {
         points: 200,
-        question:
-            'Which countries\' flags are these?',
-        imgSrc: "https://cdn.britannica.com/34/4034-050-91EE1BCF/Flag-Myanmar.jpg",
-        answer: '',
+        question: 'for how many years have i gone to hm?',
+        answer: '5',
     },
     {
         points: 300,
+        question:
+            'which countries\' flags are these?',
+        imgSrc: "https://media.istockphoto.com/id/924430118/vector/vietnam-and-south-korea-flags-vector-illustration.jpg?s=612x612&w=is&k=20&c=lnZ2pf17Y1bRNLZ22RhNwu7sejbx2_bLYESZltaE7yY=",
+        answer: 'south korea, vietnam',
+    },
+    {
+        points: 100,
         question:
             'how many siblings do i have?',
         answer: '2',
@@ -32,57 +32,74 @@ const pastQuestions: Question[] = sortQuestions([
 const presentQuestions: Question[] =
     sortQuestions([
         {
-            points: 400,
+            points: 200,
             question:
-                'This is Donu, a character from which video game?',
-            imgSrc: '/donu-gif.gif',
-            answer: 'Slay the Spire',
+                'what video game is this?',
+            imgSrc: 'qf91enynlq3hlgtkmbon.avif',
+            answer: 'rocket league',
         },
         {
             points: 100,
             question:
-                'Tahini is made from which seed?',
-            imgSrc: 'https://www.aforkstale.com/wp-content/uploads/how-to-make-homemade-tahini-1200-x-1200.jpg',
-            answer: 'Sesame',
-        },
-        {
-            points: 200,
-            question: 'What programming language is the below code?',
-            imgSrc: '/programming_language.png',
-            answer: 'Javascript',
+                'what is this game',
+            imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/4/4e/Chess_pieces_close_up.jpg?utm_source=en.wikipedia.org&utm_campaign=index&utm_content=original',
+            answer: 'chess',
         },
         {
             points: 300,
+            question: 'what neighborhood do i live in new york city?',
+            answer: 'upper west side',
+        },
+        {
+            points: 400,
             question:
-                'This country is home to the Dolomites, which are a mountain range that has historical \'via ferratas\', iron cables and rungs, to aid traversing the peaks?',
+                'what ski resort is this? hint: its in the catskills',
             imgSrc:
-                "https://laguidalpina.it/cdn/shop/products/ferrata-marmolada-cresta-ovest-Cristiano-Gregnanin-Guida-Alpina-Certificata-Dolomiti-5.jpg?v=1738870778",
-            answer: 'Italy',
+                "https://www.belleayre.com/wp-content/uploads/sites/5/fly-images/29713/Belleayre-Uphill-Trailmap-22-23-400x400-c.jpg",
+            answer: 'belleayre mountain',
         }
     ]);
 const futureQuestions: Question[] = sortQuestions([
     {
+        points: 200,
+        question:
+            'what is my rapid rating on chess.com? (rounded to the hundreds)',
+        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/4/4e/Chess_pieces_close_up.jpg?utm_source=en.wikipedia.org&utm_campaign=index&utm_content=original',
+        answer: '2000',
+    },
+    {
         points: 100,
         question:
-            'This type of 2D drawing allows you to see the sides of a 3D object at the same scale.',
-        imgSrc:
-            "https://static.mathigon.org/cms/a8141a111490d026fa6578a4933d1d47.png",
-        answer: 'Isometric',
+            'what winter sport am i doing this year?',
+        answer: 'varsity skiing',
+    },
+    {
+        points: 300,
+        question:
+            'what daw is this?',
+        imgSrc: "https://www.image-line.com/static/assets/fl-studio-screen-demo-poster.626d38c.jpg",
+        answer: 'fl studio',
+    }
+        {
+        points: 400,
+        question:
+            'what\'s my local pizza place called?',
+        answer: 'pizza pete\'s',
     }
 ]);
 
 
 const categories = [
     {
-        title: 'Ms Feng\'s Past',
+        title: 'Mikey\'s Past',
         questions: pastQuestions
     },
     {
-        title: `Ms. Feng's Present`,
+        title: `Mikey's Present`,
         questions: presentQuestions
     },
     {
-        title: "Ms. Feng's Future",
+        title: "Mikey's Future & Present 2",
         questions: futureQuestions
     }
 ];
